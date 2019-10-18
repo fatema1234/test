707757a8ebf807a56ca946c18a54417087224334
Ferdousy <panna123@yahoo.com>
2019-10-18 20:48:51 +0200

    first commit

---
+++ src/main/java/Integ3/Rogue/Actor.java
@@ -0,0 +1,27 @@
+package Integ3.Rogue;
+
+public abstract class Actor {
+	private int x;
+	private int y;
+	private int length;
+	private int width;
+	
+	public Actor(int x, int y, int length, int width) {
+		this.x = x;
+		this.y = y;
+		this.length = length;
+		this.width = width;
+		
+		
+	}
+	
+	public int getActorX() {
+		return 1;
+	}
+	
+	public int getActorY() {
+		return 1;
+	}
+	
+
+}
---
+++ src/main/java/Integ3/Rogue/ActorX.java
@@ -0,0 +1,9 @@
+package Integ3.Rogue;
+
+public class ActorX extends Actor{
+	
+	public ActorX(int x, int y, int length, int width) {
+		
+	}
+
+}
